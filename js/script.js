3610ffde6fee46323edb8e36914d7bc4e48e2f00
GaetanoNicotra <nicotragaetano10@gmail.com>
2025-03-27 16:05:51 +0100

    [UPD] Creato il ciclo per generare numeri randomic da inserire negli li

--- js/script.js
+++ js/script.js
@@ -1,13 +1,17 @@
 // RECUPERO GLI ELEMENTI DAL DOM
-debugger;
+
 let numbersList = document.getElementById('numbers-list');
-
-
 
 // CREO UN CICLO FOR PER GENERARE 5 NUMERI RANDOMICI DA INSERISRE NELLA UL
 
-for (i = 0; i <= 5; i++) {
-    let randomNumbers = Math.floor(Math.random() * 5) + 1;
-console.log(randomNumbers)
-    numbersList += `<li>${randomNumbers}</li>`
+for (i = 0; i < 5; i++) {
+
+    let randomNumbers = Math.floor(Math.random() * 98) + 1;
+    console.log(randomNumbers)
+
+    const li = document.createElement('li')
+
+    li.append(randomNumbers[i])
+    console.log(li)
+    numbersList.appendChild(li)
 }
