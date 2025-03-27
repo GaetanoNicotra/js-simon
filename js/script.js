4cc7977c2edbf20d95a47038797ddb4fb36ded24
GaetanoNicotra <nicotragaetano10@gmail.com>
2025-03-27 16:51:19 +0100

    [UPD] Completato il countdown

--- js/script.js
+++ js/script.js
@@ -7,10 +7,30 @@ let numbersList = document.getElementById('numbers-list');
 for (i = 0; i < 5; i++) {
 
     let randomNumbers = Math.floor(Math.random() * 98) + 1;
-    console.log(randomNumbers)
 
     const li = document.createElement('li')
     li.append(randomNumbers)
-    console.log(li)
     numbersList.appendChild(li)
 }
+
+// USO LE TIMING FUNCTION PER CREARE IL COUNTDOWN DI 30 SECONDI
+
+// RECUPERO O DICHIARO GLI ELEMENTI E VARIABILI CHE MI SERVONO 
+let time = 10;
+const countdown = document.getElementById('countdown')
+countdown.innerText = time --;
+const number = document.querySelectorAll('number')
+
+const countdownScreen = setInterval(function () {
+
+    if (time != -1) {
+        countdown.innerText = time
+    }
+    else {
+        clearInterval(countdownScreen)
+        numbersList.innerText = number
+
+    }
+    time--;
+}, 1000)
+
